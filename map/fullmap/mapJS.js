43bbdfcaa2a844361982e0b7e4847818cebcea8e
Antonio <ant.calatayud@gmail.com>
2015-08-13 22:09:38 +0100

    updateLayerVisibility

--- map/fullmap/mapJS.js
+++ map/fullmap/mapJS.js
@@ -243,7 +243,7 @@ var layer, legend;
 				
 				on(dom.byId("DiseasesLayersOnOff"), "change", updateLayerVisibility);
 				
-				function updateLayerVisibilit()
+				function updateLayerVisibility()
 				{
 					
 					layer.visible = false;
